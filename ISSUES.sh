ac5fc752e14d1f2dd5c4f78e59153b6a6a958e6f
John S. Urban <urbanjost@comcast.net>
2022-05-03 19:38:58 -0400

    refresh ISSUES

--- ISSUES.sh
+++ ISSUES.sh
@@ -57,12 +57,11 @@ scripts \
 index \
 $NULL
 do
-echo "
+echo "\
 |[$NAME](http://github.com/urbanjost/$NAME)|\
 [![GitHub issues open](https://img.shields.io/github/issues/urbanjost/$NAME.svg?maxAge=2)](https://github.com/urbanjost/$NAME/issues)|\
-[![GitHub stars](https://img.shields.io/github/stars/urbanjost/$NAME.svg)](https://github.com/urbanjost/$NAME/docs/man3.html)\
-|
-"
+[![GitHub stars](https://img.shields.io/github/stars/urbanjost/$NAME.svg)](https://urbanjost.github.io/$NAME/docs/man3.html)\
+|"
 done
 cat <<\EOF
 ## See Also:
