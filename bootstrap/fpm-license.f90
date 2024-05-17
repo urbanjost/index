d357b0cc3bf4fce042c473fa9ec26af0ab4ceccd
John S. Urban <urbanjost@comcast.net>
2024-05-17 17:08:10 -0400

    20240517

--- bootstrap/fpm-license.f90
+++ bootstrap/fpm-license.f90
@@ -28156,6 +28156,48 @@ help=[ CHARACTER(LEN=128) :: &
 '   Typically the output requires editing to insert specific names,dates     ',&
 '   and organizations as appropriate.                                        ',&
 '                                                                            ',&
+'   Available license descriptions include                                   ',&
+'                                                                            ',&
+'      ------------------------------------------      ----------------      ',&
+'      License                                         License keyword       ',&
+'      ------------------------------------------      ----------------      ',&
+'      Academic Free License v3.0                      AFL-3.0               ',&
+'      Apache license 2.0                              Apache-2.0            ',&
+'      Artistic license 2.0                            Artistic-2.0          ',&
+'      Boost Software License 1.0                      BSL-1.0               ',&
+'      BSD 2-clause "Simplified" license               BSD-2-Clause          ',&
+'      BSD 3-clause "New" or "Revised" license         BSD-3-Clause          ',&
+'      BSD 3-clause Clear license                      BSD-3-Clause-Clear    ',&
+'      BSD 4-clause "Original" or "Old" license        BSD-4-Clause          ',&
+'      BSD Zero-Clause license                         0BSD                  ',&
+'      Creative Commons license family                 CC                    ',&
+'      Creative Commons Zero v1.0 Universal            CC0-1.0               ',&
+'      Creative Commons Attribution 4.0                CC-BY-4.0             ',&
+'      Creative Commons Attribution ShareAlike 4.0     CC-BY-SA-4.0          ',&
+'      Do What The F*ck You Want To Public License     WTFPL                 ',&
+'      Educational Community License v2.0              ECL-2.0               ',&
+'      Eclipse Public License 1.0                      EPL-1.0               ',&
+'      Eclipse Public License 2.0                      EPL-2.0               ',&
+'      European Union Public License 1.1               EUPL-1.1              ',&
+'      GNU Affero General Public License v3.0          AGPL-3.0              ',&
+'      GNU General Public License family               GPL                   ',&
+'      GNU General Public License v2.0                 GPL-2.0               ',&
+'      GNU General Public License v3.0                 GPL-3.0               ',&
+'      GNU Lesser General Public License family        LGPL                  ',&
+'      GNU Lesser General Public License v2.1          LGPL-2.1              ',&
+'      GNU Lesser General Public License v3.0          LGPL-3.0              ',&
+'      ISC                                             ISC                   ',&
+'      LaTeX Project Public License v1.3c              LPPL-1.3c             ',&
+'      Microsoft Public License                        MS-PL                 ',&
+'      MIT                                             MIT                   ',&
+'      Mozilla Public License 2.0                      MPL-2.0               ',&
+'      Open Software License 3.0                       OSL-3.0               ',&
+'      PostgreSQL License                              PostgreSQL            ',&
+'      SIL Open Font License 1.1                       OFL-1.1               ',&
+'      University of Illinois/NCSA Open Source License NCSA                  ',&
+'      The Unlicense                                   Unlicense             ',&
+'      zLib License                                    Zlib                  ',&
+'                                                                            ',&
 'OPTIONS                                                                     ',&
 '   SPDX tagnames(s)   SPDX tagname of license to display. All supported     ',&
 '                      tag names are displayed by default.                   ',&
@@ -28177,7 +28219,7 @@ help=[ CHARACTER(LEN=128) :: &
 '                                                                            ',&
 '     fpm-license --all  # display all supported license descriptions        ',&
 '                                                                            ',&
-'     fpm-license --code  --mit # write license as Fortran code              ',&
+'     fpm-license --fortran --mit # write license as Fortran code            ',&
 '                                                                            ',&
 'SEE ALSO                                                                    ',&
 '                                                                            ',&
@@ -28186,6 +28228,8 @@ help=[ CHARACTER(LEN=128) :: &
 '    + https://docs.github.com/en/repositories/...                           ',&
 '    managing-your-repositorys-settings-and-features/...                     ',&
 '    customizing-your-repository/licensing-a-repository                      ',&
+'    + https://opensource.guide/legal/...                                    ',&
+'    #which-open-source-license-is-appropriate-for-my-project                ',&
 '']
 version=[ CHARACTER(LEN=128) :: &
 'PRODUCT:        GPF (General Purpose Fortran) utilities and examples',&
